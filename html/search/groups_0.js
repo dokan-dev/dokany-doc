f85387f932b894cc888d7cb8acaee94a1a5e5380
appveyor <appveyor@appveyor.org>
2019-05-11 21:00:48 +0000

    Latest documentation on successful appveyor build 1.2.2-641 auto-pushed to gh-pages

--- html/search/groups_0.js
+++ html/search/groups_0.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['dokan',['Dokan',['../group___dokan.html',1,'']]],
   ['dokan_5foption',['DOKAN_OPTION',['../group___d_o_k_a_n___o_p_t_i_o_n.html',1,'']]],
-  ['dokanmainresult',['DokanMainResult',['../group___dokan_main_result.html',1,'']]]
+  ['dokanmainresult',['DokanMainResult',['../group___dokan_main_result.html',1,'']]],
+  ['dokan_20notify',['Dokan Notify',['../group___dokan_notify.html',1,'']]]
 ];
