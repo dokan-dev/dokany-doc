8943dd9560e32edcfdf1fb0e650d137a4187ec56
appveyor <appveyor@appveyor.org>
2023-05-17 02:59:38 +0000

    Latest documentation on successful appveyor build 2.0.3-646 auto-pushed to gh-pages

--- html/search/variables_11.js
+++ html/search/variables_11.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['uncname',['UNCName',['../struct_d_o_k_a_n___o_p_t_i_o_n_s.html#afa11cd3d0e4af28ec8d7e2577953df69',1,'DOKAN_OPTIONS::UNCName()'],['../struct_d_o_k_a_n___i_n_s_t_a_n_c_e.html#a691f1e621408f0a9f30d38df35a20d04',1,'DOKAN_INSTANCE::UNCName()']]],
+  ['unimplementedfindfileswithpattern',['UnimplementedFindFilesWithPattern',['../struct_d_o_k_a_n___o_p_e_n___i_n_f_o.html#a5ac57423b2112d0be9eb3f8ef6783ce3',1,'DOKAN_OPEN_INFO']]],
   ['unlockfile',['UnlockFile',['../struct_d_o_k_a_n___o_p_e_r_a_t_i_o_n_s.html#a267dd56af4da19e93db535e4091d7737',1,'DOKAN_OPERATIONS']]],
   ['unmounted',['Unmounted',['../struct_d_o_k_a_n___o_p_e_r_a_t_i_o_n_s.html#a84302512a77f83d10cfd33c33f0f33a7',1,'DOKAN_OPERATIONS']]],
   ['unmountedcalled',['UnmountedCalled',['../struct_d_o_k_a_n___i_n_s_t_a_n_c_e.html#a4f975f582e7b8061b0f8571fc10df448',1,'DOKAN_INSTANCE']]],
